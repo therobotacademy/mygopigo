78b164ebb4399f65fb843f8afa3645600205ddbe
brjapon <brjapon@therobotacademy.com>
2019-08-14 20:30:44 +0200

    roslaunch file fixed

--- src/distance-sensor_subscriber.py
+++ src/distance-sensor_subscriber.py
@@ -3,17 +3,17 @@
 # BEGIN ALL
 import rospy
 from sensor_msgs.msg import Range
-# from std_msgs.msg import Int32
-
 
 # BEGIN CALLBACK
 def callback(msg):
-    print msg.data
-    rospy.loginfo(rospy.get_caller_id() + 'GoPiGo3 measures distance %s mm', msg.data*1000)
+    print "---------------------------"
+    print msg
+    print "---------------------------"
+    rospy.loginfo(rospy.get_caller_id() + ' GoPiGo3 measures distance %.1f mm', msg.range*1000)
 # END CALLBACK
 
 
-rospy.init_node('distance_sensor_subscriber')
+rospy.init_node('distance_subscriber')
 
 # BEGIN SUBSCRIBER
 sub = rospy.Subscriber('distance_sensor/distance', Range, callback)
